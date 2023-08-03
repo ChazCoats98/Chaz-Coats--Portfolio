502c8740e5ba4f81ba03eeb608af9a7673e3f40e
Chaz Coats <chazcoats2@hotmail.com>
2023-08-03 14:56:02 -0400

    Adding hiding header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,48 @@
 $(document).ready(function () {
-    gsap.registerPlugin(Draggable);
-    var slides = $("section");
-    var container = $("panelWrap");
-    var lastSlide = 0;
-    var currentSlide = 0;
-    var duration = 0.6;
-    var iHeight = window.innerHeight;
-    var offsets = [];
     var didScroll;
     var lastScrollTop = 0;
     var delta = 5;
     var headerHeight = $("header").outerHeight();
 
+    $('#panelWrap').fullpage({
+        menu: '#myNavbar',
+        anchors:['about', 'projects','resume','contact'],
+        scrollBar: false,
+        licenseKey:'ZQLIJ-PWU76-ZW2HK-3K5QI-DKKYL',
+        fitToSection: true,
+        dragAndMove: true,
+        keyboardScrolling: true,
+        animateAnchor: true,
+        lazyLoading: true,
+        beforeLeave: function() {
+            $(".headerTop").slideUp("slow", function(){
+                $(".headerTop").hide();
+            });
+            $(".headerText").slideUp("slow", function(){
+                $(".headerText").hide();
+            });
+        }
+        });
+
     $(".moreInfoBox").hide();
 
-    $(window).scroll(function (event) {
-        didScroll = true;
-        console.log("scroll down true");
-    });
-
-    setInterval(function () {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-            console.log("didscroll false");
-        }
-    }, 250);
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        if(Math.abs(lastScrollTop-st) <= delta)
-        return;
-        if (st > lastScrollTop && st > headerHeight) {
-            $("header").removeClass("scrollDown").addClass("scrollUp");
-        } else {
-            if (st + $(window).height() < $(document).height()) {
-                $("header").removeClass("scrollUp").addClass("scrollDown");
-            }
-        }
-        lastScrollTop = st;
-    }
-
-    function newSize() {
-        offsets = [];
-        ih = window.innerHeight;
-        gsap.set("#panelWrap", { height: slides.length * ih });
-        gsap.set(slides, { height: ih });
-        for (let i = 0; i < slides.length; i++) {
-          offsets.push(-slides[i].offsetTop);
-        }
-        gsap.set(container, { y: offsets[activeSlide] });
-        dragMe[0].vars.snap = offsets;
-      }
+    $("#currPage").click(function () {
+        fullpage_api.moveSectionUp();
+    })
+    $("#lastPage").click(function () {
+        fullpage_api.moveSectionDown();
+    })
 
     if ($("#hiMessage").length == 1) {
-             
+
 
         var typed = new Typed("#hiMessage", {
             strings: ["HI IM CHAZ."],
             startDelay: 1200,
-            typeSpeed:70,
+            typeSpeed: 70,
             loop: false,
             showCursor: false,
-            onComplete: function() {
+            onComplete: function () {
                 var typed = new Typed("#aboutMe", {
                     strings: ["I AM A PROGRAMMER.", "I AM AN ENGINEER.", "I AM AN ARTIST.", "I AM A MUSIC LOVER.", "I AM A WRITER.", "I AM A MOVIE FANATIC."],
                     startDelay: 100,
@@ -70,24 +51,22 @@ $(document).ready(function () {
                     backSpeed: 50,
                     loop: false,
                     showCursor: false,
-                    onComplete: function() {
+                    onComplete: function () {
                         var typed = new Typed("#more", {
                             strings: ["WANT TO KNOW MORE?"],
                             startDelay: 800,
                             typeSpeed: 70,
                             loop: false,
                             showCursor: false,
-                            onComplete: function() {
-                                $( ".typedBox").delay(1000).fadeOut("slow");
+                            onComplete: function () {
+                                $(".typedBox").delay(1000).fadeOut("slow");
                                 $(".moreInfoBox").delay(2000).fadeIn(1000);
                             }
-                    });
-                }
+                        });
+                    }
                 });
             }
         });
     }
 
-    $("#currPage").click(slideAnim);
-    $("#lastPage").click(slideAnim);
 });
