3f94dcc5363557b564c104ed521c88db5596aa52
Chaz Coats <chazcoats2@hotmail.com>
2023-08-10 10:51:46 -0400

    tweaks in css and js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,12 +25,14 @@ $(document).ready(function () {
         });
 
     $(".moreInfoBox").hide();
+    $(".jobInfoBox").hide();
     $("#job1").hide();
     $("#job2").hide();
     $("#job3").hide();
     $("#job4").hide();
     $("#job5").hide();
     $("#resume3").hide();
+    // $(".resumeBox").hide();
     $(".headerTop").hide();
 
     $("#currPage").click(function () {
@@ -76,33 +78,35 @@ $(document).ready(function () {
         });
     }
     
-    if ($("#resume1").length == 1) {
-        var typed = new Typed("#resume1", {
-            strings: ["I'VE WORKED IN A PLETHORA OF DIFFERENT INDUSTRIES WHICH HAVE TAUGHT ME A RANGE OF DIFFERENT SKILLS."],
-            startDelay: 1200,
-            typeSpeed: 70,
-            loop: false,
-            showCursor: false,
-            onComplete: function (){
-                var typed = new Typed("#resume2", {
-                    strings: ["I'VE BEEN:"],
-                    startDelay: 800,
-                    smartBackspace: true,
-                    typeSpeed: 70,
-                    backSpeed: 50,
-                    loop: false,
-                    showCursor: false,
-                    onComplete: function () {
-                        $("#job1").delay(1000).fadeIn(1000);
-                        $("#job2").delay(2500).fadeIn(1000);
-                        $("#job3").delay(4000).fadeIn(1000);
-                        $("#job4").delay(5500).fadeIn(1000);
-                        $("#job5").delay(7000).fadeIn(1000);
-                        $("#resume3").delay(8500).fadeIn(1000);
-                    }
-                });
-            }
-        })
-    }
+    // if ($("#resume1").length == 1) {
+    //     var typed = new Typed("#resume1", {
+    //         strings: ["I'VE WORKED IN A PLETHORA OF DIFFERENT INDUSTRIES WHICH HAVE TAUGHT ME A RANGE OF DIFFERENT SKILLS."],
+    //         startDelay: 1200,
+    //         typeSpeed: 70,
+    //         loop: false,
+    //         showCursor: false,
+    //         onComplete: function (){
+    //             var typed = new Typed("#resume2", {
+    //                 strings: ["I'VE BEEN:"],
+    //                 startDelay: 800,
+    //                 smartBackspace: true,
+    //                 typeSpeed: 70,
+    //                 backSpeed: 50,
+    //                 loop: false,
+    //                 showCursor: false,
+    //                 onComplete: function () {
+    //                     $("#job1").delay(1000).fadeIn(1000);
+    //                     $("#job2").delay(2500).fadeIn(1000);
+    //                     $("#job3").delay(4000).fadeIn(1000);
+    //                     $("#job4").delay(5500).fadeIn(1000);
+    //                     $("#job5").delay(7000).fadeIn(1000);
+    //                     $("#resume3").delay(8500).fadeIn(1000);
+    //                     $(".jobInfoBox").delay(14000).fadeOut("slow");
+    //                     $(".resumeBox").delay(14000).fadeIn(1000);
+    //                 }
+    //             });
+    //         }
+    //     })
+    // }
 
 });
